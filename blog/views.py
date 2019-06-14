6c872edd0f352011ab932cffdf967b42753f0ca5
ExMiracle <exmiracle2@gmail.com>
2019-06-14 18:01:19 +0300

    Added some flavour to html pages, made migration and started working on DB.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+posts = [
+        {
+            'author':'Ilia',
+            'title':'Post 1',
+            'content':'Content',
+            'date_posted':'Date',
+        },
+        {
+            'author':'Not Ilia',
+            'title':'Post 2',
+            'content':'Other content',
+            'date_posted':'Other date',
+        }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+            'posts':posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title':'About'})
 
 # Create your views here.
