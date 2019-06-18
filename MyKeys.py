537209ae95d2f834e51ed9dd790cf1d8c3aae358
ExMiracle <exmiracle2@gmail.com>
2019-06-18 16:08:27 +0300

    Password reset added. TBD: deployment, file storage (S3), encryption.

---
+++ MyKeys.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 18 12:15:43 2019
+
+@author: sail9001
+"""
+
+#https://www.youtube.com/watch?v=5iWhQWVXosU for linux
+
+import os
+
+db_user = 'me'
+db_password = 'mepass'
+
+print(db_user)
+print(db_password)
