f456b4fa2124a6634429b1b23abfc2ed12922c7c
ExMiracle <exmiracle2@gmail.com>
2019-07-21 04:21:25 +0300

    User tests done. TODO: blog tests.

--- blog/tests.py
+++ blog/tests.py
@@ -3,15 +3,11 @@ from blog.models import Post
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
-from users.forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
-# from django.core.urlresolvers import reverse
-# from blog.forms import WhateverForm
-
 from django.test import override_settings
+
 @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
 
 
-# models test
 class PostTest(TestCase):
     
     def create_user(self):
@@ -25,58 +21,35 @@ class PostTest(TestCase):
                                    content=content,
                                    author=self.create_user(),
                                    date_posted=timezone.now())
-
-    def test_post_creation(self):
-        new_post = self.create_post()
-        self.assertTrue(isinstance(new_post, Post))
+        
+    def test_post_creation(self, url='/post/new/'):
+        self.create_user()
+        data = {'title':'title',
+                'content':'Post'}
+        try_post = self.client.post(url, data)
+        self.assertRedirects(try_post, '/post/1/')
     
     def test_post_model(self):
         self.post = self.create_post()
-        self.assertEqual(self.post.title, self.post.__str__())
-    
-    
-    def test_valid_user_register_form(self):
-        data = {'username':'UserName',
-                'email':'email@email.com',
-                'password1':'12345',
-                'password2':'12345'}
-        form = UserRegisterForm(data=data)
-        self.assertFalse(form.is_valid())
-    
-    def test_valid_user_update_form(self):
-        data = {'username':'UserName',
-                'email':'email@email.com'}
-        form = UserUpdateForm(data=data)
-        self.assertTrue(form.is_valid())
+        # new_post = self.create_post()
+        self.assertTrue(isinstance(self.post, Post))
+        self.assertEqual(self.post.title, self.post.__str__())        
     
     def test_home_status_code(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-
-    def test_reverse_home_url_status_code(self):
         response = self.client.get(reverse('blog-home'))
         self.assertEqual(response.status_code, 200)
-        
+
     def test_about_status_code(self):
         response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
-    
-    def test_reverse_about_status_code(self):
         response = self.client.get(reverse('blog-about'))
         self.assertEqual(response.status_code, 200)
     
-        
-    # def test_post_list_view(self):
-    #     # w = self.create_whatever()
-    #     url = reverse("PostListView.as_view()")
-    #     resp = self.client.get(url)
-
-    #     self.assertEqual(resp.status_code, 200)
-        # self.assertIn(w.title, resp.content)
-        
-    # def test_home_page(self, context={'posts': Post.objects.all()}):
-    #     url = reverse("blog.views.home")
-    #     resp = self.client.get(url)
-    #     self.assertEqual(resp.status_code, 200)
-
-        
+    def test_new_post_status_code(self):
+        self.create_user()
+        response = self.client.get('/post/new/')
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('post-create'))
+        self.assertEqual(response.status_code, 200)
--- users/tests.py
+++ users/tests.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
-# from users.models import Profile
+from users.models import Profile
 from django.contrib.auth.models import User
+from django.test import override_settings
+from django.urls import reverse
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+@override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+
 
 class UserTest(TestCase):
     
@@ -9,11 +16,58 @@ class UserTest(TestCase):
         self.client.login(username='testuser1', password='12345')
         return User.objects.get(id=1)
     
+    def test_valid_user_update_form(self):
+        self.create_user()
+        data = {'username':'UserName',
+                'email':'email@email.com'}
+        form = UserUpdateForm(data=data)
+        self.assertTrue(form.is_valid())
+        
     def test_profile_model(self):
-        # self.user = User.objects.create_user(username='testuser', password='12345')
-        # # self.client.login(username='testuser', password='12345')
-        self.user = self.create_user()
+        self.create_user()
         self.assertEqual(self.user.username+' Profile', self.user.profile.__str__())
     
-
-# Create your tests here.
+    def test_profile_view(self):
+        self.create_user()
+        response = self.client.get(reverse('profile'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+                                'users/profile.html')
+        self.failUnless(isinstance(response.context['p_form'],
+                                   ProfileUpdateForm))
+        self.failUnless(isinstance(response.context['u_form'],
+                                   UserUpdateForm))
+    
+    def test_profile_view_post_success(self):
+        self.create_user()
+        data = {'username':'UserName',
+              'email':'email@email.com'}
+        image = SimpleUploadedFile("file.jpeg", b"file_content", content_type="image/jpeg")
+        u_form = UserUpdateForm(data=data)
+        p_form = ProfileUpdateForm(data=image)
+        self.assertTrue(u_form.is_valid())
+        self.assertTrue(p_form.is_valid())
+        response = self.client.post(reverse('profile'),
+                                    data={'username':'UserName',
+                                          'email':'email@email.com',
+                                          'image':'image.jpeg'})
+        self.assertRedirects(response, '/profile/')
+        
+    def test_register_view(self):
+        response = self.client.get(reverse('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+                                'users/register.html')
+        self.failUnless(isinstance(response.context['form'],
+                                   UserRegisterForm))
+    
+    def test_register_view_post_success(self):
+        data = {'username':'UserName',
+              'email':'email@email.com',
+              'password1':'Testing321',
+              'password2':'Testing321'}
+        form = UserRegisterForm(data=data)
+        self.assertTrue(form.is_valid())
+        response = self.client.post(reverse('register'), data=data)
+        self.assertRedirects(response, '/login/')
+        self.assertEqual(Profile.objects.count(), 1)
